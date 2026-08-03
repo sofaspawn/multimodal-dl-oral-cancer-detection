bb3c84d96ff322eebddd02ff2c4f3ec926607346
sofaspawn <h.m1nus.work@gmail.com>
2026-08-03 22:46:26 +0530

    some changes

--- backend/app/routers/prediction.py
+++ backend/app/routers/prediction.py
@@ -4,19 +4,19 @@ import shutil
 
 from uuid import uuid4
 
-extension = Path(file.filename).suffix if file.filename else ".jpg"
-
 router = APIRouter()
 
 UPLOAD_DIR = Path("app/uploads")
-
 
 @router.post("/predict")
 async def predict(file: UploadFile = File(...)):
     if file.filename is None:
         raise HTTPException(status_code=400, detail="No filename provided.")
 
-    destination = UPLOAD_DIR / file.filename
+    extension = Path(file.filename).suffix if file.filename else ".jpg"
+    filename = f"{uuid4()}{extension}"
+
+    destination = UPLOAD_DIR / filename
 
     with destination.open("wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
